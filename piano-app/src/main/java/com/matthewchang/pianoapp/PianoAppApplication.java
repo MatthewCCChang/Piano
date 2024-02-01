b1b19a9a147fd26141488f2ad8f56c315ad5320f
Matthew <deadwalker1228@gmail.com>
2024-01-31 16:42:12 -0800

    Setup

---
+++ piano-app/src/main/java/com/matthewchang/pianoapp/PianoAppApplication.java
@@ -0,0 +1,13 @@
+package com.matthewchang.pianoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PianoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PianoAppApplication.class, args);
+	}
+
+}
---
+++ piano-app/src/test/java/com/matthewchang/pianoapp/PianoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewchang.pianoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
